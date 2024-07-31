0c4c4e5cf78a45b5604a34103b7b49773fc8bb34
Vinitcodeit <vtambralli412@gmail.com>
2024-07-31 13:59:54 +0530

    updated productController component

--- controllers/productController.js
+++ controllers/productController.js
@@ -71,6 +71,7 @@ const deleteProductById = async (req, res)=>{
     if(!deletedProduct){
       return res.status(404).json({error: "No product found"})
     }
+    res.status(200).json({message:"Product deleted successfully"})
   } catch (error) {
     console.error(error);
     res.status(500).json({error:"Internal server error"})
